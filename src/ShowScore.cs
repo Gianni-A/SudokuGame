698f4471c5389504a0de3e9a120adc758a6f7c71
Gianni Alvarado <giannialvaradoc@gmail.com>
2017-05-04 21:57:47 -0500

    Add files via upload

---
+++ src/ShowScore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuGame
+{
+    class ShowScore
+    {
+        public void Mostrar(int puntuacion)
+        {
+            string show, show2, show3;
+            show = " ***************************************************\n";
+            show2 = "*           La puntuación del juego              *\n";
+            show3 = "***************************************************\n\n\n";
+            Console.SetCursorPosition((Console.WindowWidth - show.Length) / 2, Console.CursorTop);
+            Console.WriteLine(show);
+            Console.SetCursorPosition((Console.WindowWidth - show2.Length) / 2, Console.CursorTop);
+            Console.WriteLine(show2);
+            Console.SetCursorPosition((Console.WindowWidth - show3.Length) / 2, Console.CursorTop);
+            Console.WriteLine(show3);
+            Console.Write("Tu record fue de: "+ puntuacion+" puntos");
+            Console.ReadLine();
+            Console.Clear();
+            Menu backMenu = new Menu();
+            backMenu.CreaMenu();
+        }
+    }
+}
